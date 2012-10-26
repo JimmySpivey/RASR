a42af1bb2b67342cacadcacee55b7f3da29c82d6
Jimmy Dean Spivey <JimDeanSpivey@gmail.com>
2012-10-26 15:24:53 -0400

    Added Text Selection Listener

--- RASR Plugin/src/org/osehra/eclipse/atfrecorder/views/ExpectedValueView.java
+++ RASR Plugin/src/org/osehra/eclipse/atfrecorder/views/ExpectedValueView.java
@@ -3,24 +3,20 @@ package org.osehra.eclipse.atfrecorder.views;
 
 import java.util.Map;
 
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DragDetectEvent;
+import org.eclipse.swt.events.DragDetectListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.ISourceProvider;
 import org.eclipse.ui.ISourceProviderListener;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.services.ISourceProviderService;
 
@@ -49,52 +45,9 @@ public class ExpectedValueView extends ViewPart implements ISourceProviderListen
 	 */
 	public static final String ID = "org.osehra.eclipse.atfrecorder.views.OverrideExpectView";
 
-//	private TableViewer viewer;
-//	private Action action1;
-//	private Action action2;
-//	private Action doubleClickAction;
-	
 	private Text text;
-	
 	private String currentScreen;
 
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
-	 */
-	 
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-		public void dispose() {
-		}
-		public Object[] getElements(Object parent) {
-			return new String[] { "One", "Two", "Three" };
-		}
-	}
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
-	class NameSorter extends ViewerSorter {
-	}
-
-	/**
-	 * The constructor.
-	 */
 	public ExpectedValueView() {
 	}
 
@@ -103,113 +56,33 @@ public class ExpectedValueView extends ViewPart implements ISourceProviderListen
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
-//		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-//		viewer.setContentProvider(new ViewContentProvider());
-//		viewer.setLabelProvider(new ViewLabelProvider());
-//		viewer.setSorter(new NameSorter());
-//		viewer.setInput(getViewSite());
-		
 		text = new Text(parent, SWT.V_SCROLL );
 		text.setText("");
 		Device device = Display.getCurrent();
 		text.setBackground(new Color(device, 0, 0, 0));
 		text.setEditable(false);
 		text.setForeground(new Color(device, 255, 255, 255));
-		FontData fd = new FontData("Courier New", 10, SWT.BOLD);
-		text.setFont(new Font(device, fd)); //TODO: add backup fonts/test for other supported OS'es		
-		
-		// Create the help context id for the viewer's control
-//		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "ATF_Recorder_Plugin.viewer");
-//		makeActions();
-//		hookContextMenu();
-//		hookDoubleClickAction();
-//		contributeToActionBars();
-		
+		FontData fd = new FontData("Courier New", 10, 0);
+		text.setFont(new Font(device, fd)); //TODO: add backup fonts/test for other supported OS'es
+		text.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				System.out.println("widgetSelected");
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				System.out.println("widgetDefaultSelected");				
+			}
+		}); //doesn't work on selected text
+				
 		//register our Listener View (so it can get updates to the current screen)
 		ISourceProviderService service = (ISourceProviderService)getSite().getService(ISourceProviderService.class);
 		ISourceProvider screenStateProvider = service.getSourceProvider("org.osehra.rasr.sourceprovider.screen");
 		screenStateProvider.addSourceProviderListener(this);
 	}
-
-//	private void hookContextMenu() {
-//		MenuManager menuMgr = new MenuManager("#PopupMenu");
-//		menuMgr.setRemoveAllWhenShown(true);
-//		menuMgr.addMenuListener(new IMenuListener() {
-//			public void menuAboutToShow(IMenuManager manager) {
-//				ExpectedValueView.this.fillContextMenu(manager);
-//			}
-//		});
-//		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-//		viewer.getControl().setMenu(menu);
-//		getSite().registerContextMenu(menuMgr, viewer);
-//	}
-
-//	private void contributeToActionBars() {
-//		IActionBars bars = getViewSite().getActionBars();
-//		fillLocalPullDown(bars.getMenuManager());
-//		fillLocalToolBar(bars.getToolBarManager());
-//	}
-//
-//	private void fillLocalPullDown(IMenuManager manager) {
-//		manager.add(action1);
-//		manager.add(new Separator());
-//		manager.add(action2);
-//	}
-//
-//	private void fillContextMenu(IMenuManager manager) {
-//		manager.add(action1);
-//		manager.add(action2);
-//		// Other plug-ins can contribute there actions here
-//		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-//	}
-//	
-//	private void fillLocalToolBar(IToolBarManager manager) {
-//		manager.add(action1);
-//		manager.add(action2);
-//	}
-//
-//	private void makeActions() {
-//		action1 = new Action() {
-//			public void run() {
-//				showMessage("Action 1 executed");
-//			}
-//		};
-//		action1.setText("Action 1");
-//		action1.setToolTipText("Action 1 tooltip");
-//		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-//			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//		
-//		action2 = new Action() {
-//			public void run() {
-//				showMessage("Action 2 executed");
-//			}
-//		};
-//		action2.setText("Action 2");
-//		action2.setToolTipText("Action 2 tooltip");
-//		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-//				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-//		doubleClickAction = new Action() {
-//			public void run() {
-//				ISelection selection = viewer.getSelection();
-//				Object obj = ((IStructuredSelection)selection).getFirstElement();
-//				showMessage("Double-click detected on "+obj.toString());
-//			}
-//		};
-//	}
-//
-//	private void hookDoubleClickAction() {
-//		viewer.addDoubleClickListener(new IDoubleClickListener() {
-//			public void doubleClick(DoubleClickEvent event) {
-//				doubleClickAction.run();
-//			}
-//		});
-//	}
-//	private void showMessage(String message) {
-//		MessageDialog.openInformation(
-//			viewer.getControl().getShell(),
-//			"Override Expect View",
-//			message);
-//	}
 
 	/**
 	 * Passing the focus request to the viewer's control.
@@ -240,13 +113,8 @@ public class ExpectedValueView extends ViewPart implements ISourceProviderListen
 					Display.getDefault().asyncExec(new Runnable() {
 						public void run() {
 							text.setText(currentScreen);
-							//System.out.println("text size as point: " +text.getSize());
-							System.out.println(text.getCharCount());
-							System.out.println(currentScreen.length());
-							System.out.println();
 							int offset = text.getCharCount() - currentScreen.length();
 							text.setSelection(offset+Math.max(1, currentScreen.length() - 21), offset+currentScreen.length() );
-							//text.setSelection(1);
 							text.showSelection();
 						    while (!text.isDisposed()) {
 						        if (!Display.getDefault().readAndDispatch())
